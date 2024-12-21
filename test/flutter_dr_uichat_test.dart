c8ab0c1b151b1b754f0f1d470191352ad76739e7
Darith <darith@Dariths-Mac-mini.local>
2024-12-21 22:47:11 +0700

    add

---
+++ test/flutter_dr_uichat_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_dr_uichat/flutter_dr_uichat.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
